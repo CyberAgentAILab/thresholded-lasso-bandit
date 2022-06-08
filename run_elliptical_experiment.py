927cb92770ae421a8c5036d4760441a8d8d25347
bakanaouji <abeken1224@yahoo.co.jp>
2022-06-09 01:23:18 +0900

    Delete unnecessary lines

--- run_elliptical_experiment.py
+++ run_elliptical_experiment.py
@@ -11,44 +11,41 @@ from envs.elliptical import Elliptical
 warnings.filterwarnings(action='ignore')
 
 
-def run_exp(num_trial, K, T, d, s0, l, algs):
-    trajectories = [defaultdict(list) for _ in algs]
+def run_exp(num_trial, K, T, d, s0, l, alg):
+    trajectory = defaultdict(list)
 
     for n in range(num_trial):
         print('==== Run trial {} ===='.format(n))
         rng = np.random.RandomState(np.random.randint(0, 2 ** 32))
-        alg_ins = [algs[i][0](rng, **algs[i][1]) for i in range(len(algs))]
-        cum_rewards = [0 for _ in range(len(algs))]
-        cum_regrets = [0 for _ in range(len(algs))]
-        log = [defaultdict(list) for _ in range(len(algs))]
+        alg_ins = alg[0](rng, **alg[1])
+        cum_reward = 0
+        cum_regret = 0
+        log = defaultdict(list)
 
         # run each trial
         env = Elliptical(K, d, s0, l, rng)
         for t in range(T):
             if t % 100 == 0:
-                cum_regrets_str = ', '.join(['{} regret: {}'.format(algs[i][0].__name__, cum_regrets[i]) for i in range(len(alg_ins))])
-                print('trial: {}, round: {}, {}'.format(n, t, cum_regrets_str))
+                print('trial: {}, round: {}, regret: {}'.format(n, t, cum_regret))
             # receive context set
             x = env.context()
-            for i in range(len(alg_ins)):
-                # pull arm and observe reward
-                action = alg_ins[i].choose_action(x, t + 1)
-                reward, regret = env.pull(action)
-                # update policy
-                alg_ins[i].update_beta(reward, t + 1)
-                # record log
-                cum_rewards[i] += reward
-                cum_regrets[i] += regret
-                log[i]['rewards'].append(cum_rewards[i])
-                log[i]['regrets'].append(cum_regrets[i])
-                log[i]['false_negative'].append(env.false_negative(alg_ins[i].beta))
-                log[i]['false_positive'].append(env.false_positive(alg_ins[i].beta))
-                log[i]['error_l1'].append(env.error_l1(alg_ins[i].beta))
-                log[i]['error_l2'].append(env.error_l2(alg_ins[i].beta))
-        for i in range(len(algs)):
-            for k, v in log[i].items():
-                trajectories[i][k].append(v)
-    return trajectories
+            # pull arm and observe reward
+            action = alg_ins.choose_action(x, t + 1)
+            reward, regret = env.pull(action)
+            # update policy
+            alg_ins.update_beta(reward, t + 1)
+            # record log
+            cum_reward += reward
+            cum_regret += regret
+            log['rewards'].append(cum_reward)
+            log['regrets'].append(cum_regret)
+            log['false_negative'].append(env.false_negative(alg_ins.beta))
+            log['false_positive'].append(env.false_positive(alg_ins.beta))
+            log['error_l1'].append(env.error_l1(alg_ins.beta))
+            log['error_l2'].append(env.error_l2(alg_ins.beta))
+        for k, v in log.items():
+            trajectory[k].append(v)
+    return trajectory
 
 
 def main():
@@ -70,24 +67,21 @@ def main():
     s0 = args.s0
     l = args.l
 
-    # define algorithms
-    algs = [
-        (THLassoBandit, {'K': K, 'd': d, 'lam0': 0.02})
-    ]
+    # define algorithm
+    alg = (THLassoBandit, {'K': K, 'd': d, 'lam0': 0.02})
 
     # run experiments
     print('===== Run experiments over {} trials ====='.format(args.num_trial))
-    trajectories = run_exp(args.num_trial, K, T, d, s0, l, algs)
+    trajectory = run_exp(args.num_trial, K, T, d, s0, l, alg)
 
     # save log
-    for i in range(len(algs)):
-        save_path = 'log/elliptical/K{}_d{}_s{}_l{}/{}/'.format(K, d, s0, l, algs[i][0].__name__)
-        if not os.path.isdir(save_path):
-            os.makedirs(save_path)
-        for k, v in trajectories[i].items():
-            df = pd.DataFrame(np.array(v).T)
-            df.index.name = '#index'
-            df.to_csv(save_path + k + '.csv')
+    save_path = 'log/elliptical/K{}_d{}_s{}_l{}/'.format(K, d, s0, l)
+    if not os.path.isdir(save_path):
+        os.makedirs(save_path)
+    for k, v in trajectory.items():
+        df = pd.DataFrame(np.array(v).T)
+        df.index.name = '#index'
+        df.to_csv(save_path + k + '.csv')
 
 
 if __name__ == '__main__':
--- run_gaussian_experiment.py
+++ run_gaussian_experiment.py
@@ -11,44 +11,41 @@ from envs.gaussian import Gaussian
 warnings.filterwarnings(action='ignore')
 
 
-def run_exp(num_trial, K, T, d, s0, x_max, rho_sq, algs):
-    trajectories = [defaultdict(list) for _ in algs]
+def run_exp(num_trial, K, T, d, s0, x_max, rho_sq, alg):
+    trajectory = defaultdict(list)
 
     for n in range(num_trial):
         print('==== Run trial {} ===='.format(n))
         rng = np.random.RandomState(np.random.randint(0, 2 ** 32))
-        alg_ins = [algs[i][0](rng, **algs[i][1]) for i in range(len(algs))]
-        cum_rewards = [0 for _ in range(len(algs))]
-        cum_regrets = [0 for _ in range(len(algs))]
-        log = [defaultdict(list) for _ in range(len(algs))]
+        alg_ins = alg[0](rng, **alg[1])
+        cum_reward = 0
+        cum_regret = 0
+        log = defaultdict(list)
 
         # run each trial
         env = Gaussian(K, d, s0, x_max, rho_sq, rng)
         for t in range(T):
             if t % 100 == 0:
-                cum_regrets_str = ', '.join(['{} regret: {}'.format(algs[i][0].__name__, cum_regrets[i]) for i in range(len(alg_ins))])
-                print('trial: {}, round: {}, {}'.format(n, t, cum_regrets_str))
+                print('trial: {}, round: {}, regret: {}'.format(n, t, cum_regret))
             # receive context set
             x = env.context()
-            for i in range(len(alg_ins)):
-                # pull arm and observe reward
-                action = alg_ins[i].choose_action(x, t + 1)
-                reward, regret = env.pull(action)
-                # update policy
-                alg_ins[i].update_beta(reward, t + 1)
-                # record log
-                cum_rewards[i] += reward
-                cum_regrets[i] += regret
-                log[i]['rewards'].append(cum_rewards[i])
-                log[i]['regrets'].append(cum_regrets[i])
-                log[i]['false_negative'].append(env.false_negative(alg_ins[i].beta))
-                log[i]['false_positive'].append(env.false_positive(alg_ins[i].beta))
-                log[i]['error_l1'].append(env.error_l1(alg_ins[i].beta))
-                log[i]['error_l2'].append(env.error_l2(alg_ins[i].beta))
-        for i in range(len(algs)):
-            for k, v in log[i].items():
-                trajectories[i][k].append(v)
-    return trajectories
+            # pull arm and observe reward
+            action = alg_ins.choose_action(x, t + 1)
+            reward, regret = env.pull(action)
+            # update policy
+            alg_ins.update_beta(reward, t + 1)
+            # record log
+            cum_reward += reward
+            cum_regret += regret
+            log['rewards'].append(cum_reward)
+            log['regrets'].append(cum_regret)
+            log['false_negative'].append(env.false_negative(alg_ins.beta))
+            log['false_positive'].append(env.false_positive(alg_ins.beta))
+            log['error_l1'].append(env.error_l1(alg_ins.beta))
+            log['error_l2'].append(env.error_l2(alg_ins.beta))
+        for k, v in log.items():
+            trajectory[k].append(v)
+    return trajectory
 
 
 def main():
@@ -73,24 +70,21 @@ def main():
     x_max = args.x_max
     rho_sq = args.rho_sq
 
-    # define algorithms
-    algs = [
-        (THLassoBandit, {'K': K, 'd': d, 'lam0': 0.02})
-    ]
+    # define algorithm
+    alg = (THLassoBandit, {'K': K, 'd': d, 'lam0': 0.02})
 
     # run experiments
     print('===== Run experiments over {} trials ====='.format(args.num_trial))
-    trajectories = run_exp(args.num_trial, K, T, d, s0, x_max, rho_sq, algs)
+    trajectory = run_exp(args.num_trial, K, T, d, s0, x_max, rho_sq, alg)
 
     # save log
-    for i in range(len(algs)):
-        save_path = 'log/gaussian/K{}_d{}_s{}_xm{}_rho{}/{}/'.format(K, d, s0, x_max, rho_sq, algs[i][0].__name__)
-        if not os.path.isdir(save_path):
-            os.makedirs(save_path)
-        for k, v in trajectories[i].items():
-            df = pd.DataFrame(np.array(v).T)
-            df.index.name = '#index'
-            df.to_csv(save_path + k + '.csv')
+    save_path = 'log/gaussian/K{}_d{}_s{}_xm{}_rho{}/'.format(K, d, s0, x_max, rho_sq)
+    if not os.path.isdir(save_path):
+        os.makedirs(save_path)
+    for k, v in trajectory.items():
+        df = pd.DataFrame(np.array(v).T)
+        df.index.name = '#index'
+        df.to_csv(save_path + k + '.csv')
 
 
 if __name__ == '__main__':
